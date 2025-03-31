f01845767080fba14e71cc49972859c252ae6e7e
Omer F. Erdem <50739592+omerferdem@users.noreply.github.com>
2025-03-31 10:17:03 -0400

    Update Model_Grid_Tuner.py

--- Model_Training/Grid_tuning/ConcNN_and_LUT-NN_Second_Layer_Hypertune/Model_Grid_Tuner.py
+++ Model_Training/Grid_tuning/ConcNN_and_LUT-NN_Second_Layer_Hypertune/Model_Grid_Tuner.py
@@ -7,10 +7,10 @@ import multiprocessing
 from sklearn.metrics import r2_score
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.model_selection import train_test_split
-from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Dense, Concatenate, Lambda
+from keras.models import Model
+from keras.layers import Input, Dense, Concatenate, Lambda
 from keras.callbacks import ReduceLROnPlateau
-from tensorflow.keras.optimizers import Adam
+from keras.optimizers import Adam
 import os
 
 os.environ["OMP_NUM_THREADS"] = "1"
